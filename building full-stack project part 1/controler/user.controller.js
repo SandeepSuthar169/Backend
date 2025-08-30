24f7638d32b2d9f3229829c6ff77be5992bbc154
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-08-31 01:06:06 +0530

    validate of name, email and password

--- building full-stack project part 1/controler/user.controller.js
+++ building full-stack project part 1/controler/user.controller.js
@@ -1,5 +1,20 @@
 const registerUser = async (req, res) => {
-    res.send("registered");
+    // get data
+    // validate
+    // check if user already exists
+    // crete a carifiacation token
+    // save token in dataBase
+    // send taken as emil to user
+    // send success status to user
+
+    const {name, email, password} = req.body
+    if (!name || !email || !password){
+        return res.status(400).json({
+            message: "All fields are required"
+        });
+    };
+    console.log(email);
+    
 };
 
 
