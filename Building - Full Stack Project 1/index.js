81ede7061e409fd4c1314d7b4032fbcd5d909682
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-14 22:34:24 +0530

    commit

---
+++ Building - Full Stack Project 1/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get("/sandeep", (req, res) => {
+    res.send("hellow sandeep!")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
