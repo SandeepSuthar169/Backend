eab821fc643d89b50439f9f1e2f88bddf7b2cc74
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-14 23:58:22 +0530

    connected to mongodb database

--- Building - Full Stack Project 1/index.js
+++ Building - Full Stack Project 1/index.js
@@ -1,12 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import db from "./utils/db.js"
 
 dotenv.config()
 
 const app = express()
-app.use(cors({
-    origin:""
+app.use(cors(
+    {
+    origin:process.env.BASE_URL,
+    Credential:true,
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders:['Content-Type', 'Authorization']
 }))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
 const port = process.env.PORT || 4000
@@ -20,6 +28,7 @@ app.get("/sandeep", (req, res) => {
 })
 
 
+db()
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
