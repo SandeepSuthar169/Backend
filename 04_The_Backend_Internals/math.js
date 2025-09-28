af5e0214bef42050a3bd063d352d1f6f0cef358d
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-28 14:25:18 +0530

    behind the express

---
+++ 04_The_Backend_Internals/math.js
@@ -0,0 +1,16 @@
+function add(x, y) {
+    return x + y
+}
+
+function sub(a, b) {
+    return a - b
+}
+
+
+module.exports = {
+    add, 
+    sub
+}
+
+// Named exports vs Defaults exports
+// only one file have one deafult exports and but one file have  multi named exports.
