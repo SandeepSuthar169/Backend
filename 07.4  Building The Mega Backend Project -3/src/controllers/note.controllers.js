fd899028cec03d305e7c514415654cdb82ff06b3
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-10-24 10:50:19 +0530

    *

--- 07.4  Building The Mega Backend Project -3/src/controllers/note.controllers.js
+++ 07.4  Building The Mega Backend Project -3/src/controllers/note.controllers.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 import { Project } from "../models/project.models.js"
 import { ApiError } from "../utils/api-error.js"
 import { ApiResponse } from "../utils/api-response.js"
-
+import { ProjectNote } from "../models/note.models.js"
+import { asyncHandler } from "../utils/async-handler.js"
+ 
 const getNotes = async(req, res) => {
     const { projectId } = req.params
 
@@ -13,9 +15,14 @@ const getNotes = async(req, res) => {
     }
 
     const note = await ProjectNote.find({
-        project: mongoose.Types.ObjectId(projectId)
+        project: projectId
 
     }).populate("createdBy", "username fullName avatar")
+
+    if(!note){
+        throw new ApiError(404, "project note not found ")
+    }
+
 
     return res
         .status(200)
@@ -24,9 +31,73 @@ const getNotes = async(req, res) => {
 
 const getNotesById = async(req, res) => {
 
+    const { noteId } = req.params
+
+    const note = ProjectNote.findById(noteId).populate("createdBy", "username fullname avatar")
+
+    if(!note){
+        throw new ApiError(404, "note not found")
+    }
+
+    return res.status(200).json(new ApiResponse(200, note, "Note fectch successfully"))
+
 }
 
 const createNote = async(req, res) => {
+    const { projectId } = req.params
+    const { content } = req.body
+
+    const project = await Project.findById(projectId)
+
+    if(!project){
+        throw new ApiError("404", "project not fount")
+    }
+
+    const note = ProjectNote.create({
+        project: projectId,
+        content,
+        createdBy: req.user._id
+    })
+
+    const populatedNote =await ProjectNote.findById(note._id).populate("createdBy", "username fullname avatar")
+
+    return res.status(200).json(new ApiResponse(200, populatedNote, "Note created successfully"))
+}
+
+const updateNote = async(req, res) => {
+    const { noteId } = req.params
+    const { content } = req.body
+
+    const existingNote = ProjectNote.findById(noteId)
+
+    if(!existingNote){
+        throw new ApiError(404, "exisiting note not found")
+    }
+
+    const note = ProjectNote.findByIdAndUpdate(
+        noteId,
+        {content},
+        {new: true}
+    ).populate("createdBy", "username fullname avatar")
+
+    return res.status(200).json(new ApiResponse(200, note, "update successfully"))
 
 }
+
+
+const deleteNote = async(req, res) => {
+    const { noteId } = req.params
+    
+    const delNote  = ProjectNote.findByIdAndDelete(noteId)
+
+    if(!delNote){
+        throw new ApiError(404, "note delete not found")
+    }
+
+    return res.status(200).json(new ApiResponse(200, delNote, "delete not successfully"))
+
+
+}
+
+export { getNotes, getNotesById, createNote, updateNote, deleteNote }
 
--- 07.4  Building The Mega Backend Project -3/src/routes/note.routes.js
+++ 07.4  Building The Mega Backend Project -3/src/routes/note.routes.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
-import { UserRolesEnum } from "../utils/constants.js";
+import { UserRolesEnum, AvailableUserRoles } from "../utils/constants.js";
 import { validateProjectPermission } from "../middlewares/auth.middleware.js";
+import { deleteNote, getNoteById } from "c:/Users/Sande/AppData/Local/Temp/198735cc-52d8-429c-9405-59a94cd6e768_cohort_project_camp_lyst1756965751951.zip.768/cohort-project-camp/cohort-camp-backend/src/controllers/note.controllers.js";
+import { updateNote } from "../controllers/note.controllers.js";
 
 const router = Router()
 router.route("/:projectId")
     .get(
-        validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]),
+        validateProjectPermission(AvailableUserRoles),
         getNotes)
     .post(
-        validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]),
+        validateProjectPermission([UserRolesEnum.ADMIN]),
         createNote)
+
+
+router.route("/:projectId/n/:noteId")
+    .get(validateProjectPermission(AvailableUserRoles), 
+        getNoteById)
+    .put(validateProjectPermission([UserRolesEnum.ADMIN]),
+        updateNote)
+    .delete(validateProjectPermission([UserRolesEnum.ADMIN]),
+        deleteNote)
 
 export default router
