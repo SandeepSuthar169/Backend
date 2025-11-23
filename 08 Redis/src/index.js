4e79d2e10f6a7d28977e3777daba77d9865e78e1
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-11-24 00:24:59 +0530

    redic complited?

--- 08 Redis/src/index.js
+++ 08 Redis/src/index.js
@@ -1,26 +1,64 @@
 import express from "express"
 import axios from "axios"
+import Redis from "ioredis"
 
 const app = express()
 const PORT = process.env.PORT ?? 8000
+
+
+
+const redis = new Redis({
+    host: "localhost",
+    port: Number(6379)
+})
+
 
 app.get("/", (req, res) => {
     return res.json({stauts : "success"});
 })
 
-app.get('/books', async (req, res) => {
+app.use(async function (req, res, next){
+    const key = `rat-limit`;                // const key = `rat-limit${UserId}`;
+    const value = await redis.get(key)
+
+    if(value === null){
+        await redis.set(key, 0)
+        await redis.expire(key, 60)   // 60 sec
+    }
+
+    if(value > 10 ){
+        return res.status(429).json({
+            message: "Tpp many requests"
+        }) 
+    } 
+
+    redis.incr(key);
+    next()
+})
+
+app.get('/books/total', async (req, res) => {
+    const cachedValue = await redis.get('totalPagesCount')
+    if(cachedValue){
+        console.log(`Cache Hit`);
+        
+        return res.json({ totalPages: Number(cachedValue) });
+    
+    }
+
     const response = await axios.get('https://api.freeapi.app/api/v1/public/books')
 
-    const books = response?.data?.data?.data 
+    const totalPagesCount = response?.data?.data?.data?.reduce(
+        (acc, curr) => { const pages = curr.volumeInfo?.pageCount ?? 0;
+    
+            return acc + pages;
+        }, 0);
 
-    const total = books.reduce((acc, curr) => {
-        const pages = curr.volumeInfo?.pageCount ?? 0;
-        return acc + pages;
-    }, 0);
+        console.log(`Cache Miss`);
 
-    console.log(total);
+        await redis.set("totalPagesCount", totalPagesCount)
 
-    return res.json({ totalPages: total, books });
+
+    return res.json({ totalPages:totalPagesCount });
 });
 
 
